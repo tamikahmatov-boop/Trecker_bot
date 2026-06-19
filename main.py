eab4e1a37e25226359f24102033fc4784c29b382
tamikahmatov-boop <tamikahmatov@gmail.com>
2026-06-19 14:49:31 +0300

    Refactor Telegram bot to use async functions

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+import asyncio
 from telegram import Bot
 
-# ВСТАВЬ СВОИ ДАННЫЕ
 BOT_TOKEN = "8626739818:AAFt7kmdfTgTVlXD-5FnKOVYq1fvNW9hUAw"
 CHAT_ID = 6716942872  # без кавычек
 
-main = Bot(token=BOT_TOKEN)
+async def main():
+    bot = Bot(token=BOT_TOKEN)
 
-try:
-    msg = main.send_message(
-        chat_id=CHAT_ID,
-        text="✅ Тестовое уведомление\nБот работает."
-    )
+    try:
+        msg = await bot.send_message(
+            chat_id=CHAT_ID,
+            text="✅ Тестовое уведомление\nБот работает."
+        )
 
-    print("Сообщение отправлено. ID:", msg.message_id)
+        print("Сообщение отправлено. ID:", msg.message_id)
 
-except Exception as e:
-    print("Ошибка Telegram:", e)
+    except Exception as e:
+        print("Ошибка Telegram:", e)
+
+asyncio.run(main())
